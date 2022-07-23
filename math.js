e47fe6ab630c2bbe0217478f31f3f68616618161
ErikVasconcelos <erikk43@hotmail.com>
2022-07-22 22:12:06 -0300

    More files

---
+++ math.js
@@ -0,0 +1,12 @@
+/* Math - Ferramenta bastante utilizada devido a sua grande quantidade de propriedades contidas nele
+
+
+*/
+
+console.log(Math.PI) /* Mostra o valor de PI */
+console.log(Math.pow(2,4)) /* Mostra a potência de um número, sendo o primeiro número a base, segundo número a potência */
+console.log(Math.sqrt(25,2)) /* Mostra a raiz quadrada ou cúbica de um valor, o primeiro valor está dentro da raiz, o segundo valor (2 - raiz quadrada)
+e (3 - raiz cúbica) */
+console.log(Math.random()) /* Mostra um número aleatório entre 0 e 1 */
+console.log(Math.ceil(23.6)) /* Arredonda o número mais próximo para cima */
+console.log(Math.floor(23.4)) /* Arredonda o número mais próximo para baixo */
