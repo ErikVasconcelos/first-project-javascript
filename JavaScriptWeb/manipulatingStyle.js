05cf851602fb1b2b4f025b2e5fcc4d00eaf84ee7
ErikVasconcelos <erikk43@hotmail.com>
2022-10-11 09:28:59 -0300

    Add informations

--- JavaScriptWeb/manipulatingStyle.js
+++ JavaScriptWeb/manipulatingStyle.js
@@ -1,1 +1,14 @@
-const element = document.querySelector
+const element = document.querySelector('h1')
+
+element.style.backgroundColor = 'green'
+element.style.color = 'white'
+element.style.fontWeight = 40
+
+// Utilizar o elemento.style.algumacoisa, sempre com a camelCase.
+
+// Relembrando:
+
+// camelCase - a partir da primeira palavra as primeiras letras de outras palavras são maiúsculas
+// PascalCase - a primeira palavra de todas as palavras são maiúsculas
+// snake_case - sempre haverá um underline separando as palavras
+// kebab-case - sempre haverá um hifen separando as palavras
